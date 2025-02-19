9113989d6c671646405322073ade5c199ad3cdd3
Hari <hsaravanan@my.bcit.ca>
2025-02-18 20:00:25 -0800

    made changes

--- scripts/firebaseAPI_template_TEAM99.js
+++ scripts/firebaseAPI_template_TEAM99.js
@@ -2,13 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+        apiKey: "AIzaSyB8ZvtRzagJyIOf5VkpxYCAXEC_97RJXbE",
+        authDomain: "comp1800-demo-2025.firebaseapp.com",
+        projectId: "comp1800-demo-2025",
+        storageBucket: "comp1800-demo-2025.firebasestorage.app",
+        messagingSenderId: "152376644743",
+        appId: "1:152376644743:web:d4bb55847307e2aa269422"
+      };
 
 //--------------------------------------------
 // initialize the Firebase app
